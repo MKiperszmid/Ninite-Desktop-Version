f62c1822a45bf057363df618ec1a54df49304027
BoliBerrys <kiper30@gmail.com>
2017-07-08 21:08:40 -0300

    Release
    Ninite Desktop Version release

---
+++ Ninite Desktop Version/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace NiniteClone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Install";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(82, 37);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(119, 23);
+            this.progressBar2.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(82, 9);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(119, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(56, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Steam";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Putty";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 9;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 68);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(65, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "LoL MM";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(82, 65);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(119, 23);
+            this.progressBar3.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 15;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.Location = new System.Drawing.Point(82, 94);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(119, 23);
+            this.progressBar4.TabIndex = 14;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 97);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(57, 17);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "Github";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 18;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.Location = new System.Drawing.Point(82, 123);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(119, 23);
+            this.progressBar5.TabIndex = 17;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(6, 126);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(47, 17);
+            this.checkBox5.TabIndex = 16;
+            this.checkBox5.Text = "7Zip";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 311);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ninite Desktop Version";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.CheckBox checkBox5;
+    }
+}
+
---
+++ Ninite Desktop Version/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace NiniteClone
+{
+    public partial class Form1 : Form
+    {
+        private readonly List<string> _programs = new List<string>();
+
+        private readonly List<string> _urls = new List<string> { "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi",
+            "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe",
+            "https://github.com/dewster/lol-mastery-manager-new-client/releases/download/1.2.2/LoLMasteryManagerSetup.msi",
+        "https://desktop.githubusercontent.com/releases/0.6.2-e2d9e7b3/GitHubDesktopSetup.exe",
+        "http://www.7-zip.org/a/7z1700-x64.exe"};
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public void AppInstaller(string path)
+        {
+            var startInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                FileName = "CMD.exe"
+            };
+
+            var extension = Path.GetExtension(path);
+            var name = Path.GetFileName(path);
+            
+            if (extension != null && extension.ToLower() == ".msi")
+            {
+                startInfo.Arguments = $"/c msiexec /i {name} /qn+ /norestart";
+            }
+            else if (extension != null && extension.ToLower() == ".exe")
+            {
+                startInfo.Arguments = $"/c {name} /S /norestart";
+            }
+            else
+            {
+                MessageBox.Show($@"Extension {extension} is not supported.", @"Not Supported");
+                return;
+            }
+
+            var p = new Process {StartInfo = startInfo};
+            p.Start();
+        }
+        public void DownloadApps()
+        {
+            foreach (var program in _programs)
+            {
+                var called = false;
+                var prog = progressBar1;
+                var lbl = label1;
+
+                if (_urls[0] == program)
+                {
+                    prog = progressBar1;
+                    lbl = label1;
+                }
+                if (_urls[1] == program)
+                {
+                    prog = progressBar2;
+                    lbl = label2;
+                }
+                if (_urls[2] == program)
+                {
+                    prog = progressBar3;
+                    lbl = label3;
+                }
+                if (_urls[3] == program)
+                {
+                    prog = progressBar4;
+                    lbl = label4;
+                }
+                if (_urls[4] == program)
+                {
+                    prog = progressBar5;
+                    lbl = label5;
+                }
+
+                var wc = new WebClient();
+                var name = Path.GetFileName(program);
+                wc.DownloadFileAsync(new Uri(program), $"{name}");
+                wc.DownloadProgressChanged += (object sender, DownloadProgressChangedEventArgs e) =>
+                {
+                    prog.Value = e.ProgressPercentage;
+                    lbl.Text = e.ProgressPercentage + "%";
+                    if (e.ProgressPercentage != 100 || called) return;
+                    called = true;
+                    new Thread(() => { MessageBox.Show($@"Installing {program}", @"Installing"); }).Start();
+                    AppInstaller(program);
+                };
+            }
+        }
+
+        #region List Functions
+
+        public void AddApp(string path)
+        {
+            _programs.Add(path);
+        }
+
+        public void ClearList()
+        {
+            _programs.Clear();
+        }
+
+        #endregion List Functions
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearList();
+            if(checkBox1.Checked)
+                AddApp(_urls[0]);
+            if(checkBox2.Checked)
+                AddApp(_urls[1]);
+            if(checkBox3.Checked)
+                AddApp(_urls[2]);
+            if(checkBox4.Checked)
+                AddApp(_urls[3]);
+            if(checkBox5.Checked)
+                AddApp(_urls[4]);
+            
+            DownloadApps();
+        }
+    }
+}
